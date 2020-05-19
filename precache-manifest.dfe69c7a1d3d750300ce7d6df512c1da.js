071ab2cdd4a2d5d6d20b0e85b6669ab71aebc4b7
Prithvi Anil Kumar <thepirateking3d2y@gmail.com>
2020-05-19 16:36:33 +0530

    Updates

---
+++ precache-manifest.dfe69c7a1d3d750300ce7d6df512c1da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3604b84cfc883e2e5b77e6d912ffddd2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e843f063bfda0a92dcb",
+    "url": "/static/css/main.9cf3b659.chunk.css"
+  },
+  {
+    "revision": "a0be44971a8414450501",
+    "url": "/static/js/2.7ea460d4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7ea460d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e843f063bfda0a92dcb",
+    "url": "/static/js/main.8c4df4a6.chunk.js"
+  },
+  {
+    "revision": "e753cdb1025fee5680ec",
+    "url": "/static/js/runtime-main.eaf7a164.js"
+  }
+]);
